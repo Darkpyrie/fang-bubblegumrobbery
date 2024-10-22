560aba2f9e820e7df75bb5d9967a681ef67b3389
Fang <128187114+F4ngFantasy@users.noreply.github.com>
2024-10-23 00:36:30 +1100

    Update thanks to Snowy

--- server/server.lua
+++ server/server.lua
@@ -6,36 +6,38 @@ lib.callback.register('fang-bubblegumrobbery:server:getRobbedMachines', function
     return robbedMachines[machineEntity]
 end)
 
-lib.callback.register('fang-bubblegumrobbery:server:setRobbedStatus', function(source, entityID, state)
-    if robbedMachines[entityID] then
+lib.callback.register('fang-bubblegumrobbery:server:setRobbedStatus', function(source, entity, state)
+    local model = entity.entity
+    if robbedMachines[model] ~= nil then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with robbed machine')
         return false
     end
-    if #(GetEntityCoords(GetPlayerFromServerId(source)) - GetEntityCoords(entityID)) > config.Distance then
+    if #(GetEntityCoords(GetPlayerPed(source)) - entity.coords) > config.Distance then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with invalid distance')
         return false
     end
 
-    robbedMachines[entityID] = state
-    if state and not resetTimers[entityID] then
-        resetTimers[entityID] = lib.timer((config.Cooldown * 1000), function()
-            robbedMachines[entityID] = nil
-            resetTimers[entityID] = nil 
+    robbedMachines[model] = state
+    if state and not resetTimers[model] then
+        resetTimers[model] = lib.timer((config.Cooldown * 1000), function()
+            robbedMachines[model] = nil
+            resetTimers[model] = nil 
         end, true)
     end
 
-    return (robbedMachines[entityID] == state)
+    return (robbedMachines[model] == state)
 end)
 
-lib.callback.register('fang-bubblerobbery:server:giveItem', function(source, entity)
+lib.callback.register('fang-bubblerobbery:server:giveItem', function(source, model)
+    local entity = model.entity
     local items = config.Items
     local itemSelect = math.random(1,5)
     local prizeItem = items[itemSelect].item
-    if robbedMachines[entity] then
+    if robbedMachines[entity] ~= nil then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with robbed machine')
         return false
     end
-    if #(GetEntityCoords(GetPlayerFromServerId(source)) - GetEntityCoords(entity)) > config.Distance then
+    if #(GetEntityCoords(GetPlayerPed(source)) - model.coords) > config.Distance then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with invalid distance')
         return false
     end
@@ -49,12 +51,12 @@ lib.callback.register('fang-bubblerobbery:server:giveItem', function(source, ent
     exports.ox_inventory:AddItem(source, prizeItem, items[itemSelect].count)
 end)
 
-lib.callback.register('fang-bubblerobbery:server:giveMoney', function(source, entity)
-    if robbedMachines[entity] then
+lib.callback.register('fang-bubblerobbery:server:giveMoney', function(source, model)
+    --[[if robbedMachines[model.entity] ~= nil then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with robbed machine')
         return false
-    end
-    if #(GetEntityCoords(GetPlayerFromServerId(source)) - GetEntityCoords(entity)) > config.Distance then
+    end]]
+    if #(GetEntityCoords(GetPlayerPed(source)) - model.coords) > config.Distance then
         lib.print.warn('[WARNING] Player triggered giveMoney callback with invalid distance')
         return false
     end
